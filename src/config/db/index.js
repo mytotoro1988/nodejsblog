927ea941f5f98e9b15ae11a3b4330dfbfb816edf
Mei <lehuucuong.vnua@gmail.com>
2021-01-19 09:11:36 +0700

    add lan dau

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
+    await mongoose.connect('mongodb://localhost:27017/f8_education_dev_copy', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
-    console.log('connect successful!!!');
+    console.log('Kết nối thành công !!!');
   } catch (error) {
-    console.log('connect false!!!');
+    console.log('Kết nối thất bại !!!');
   }
 }
 
